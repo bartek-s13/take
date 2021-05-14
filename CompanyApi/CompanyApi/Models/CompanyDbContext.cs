6722ad085035369135089ca82b3317e08ee87982
bartek-s13 <barteksmolski@yahoo.com>
2021-05-14 12:49:20 +0200

    lab9

---
+++ CompanyApi/CompanyApi/Models/CompanyDbContext.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace CompanyApi.Models
+{
+    public partial class CompanyDbContext : DbContext
+    {
+        public CompanyDbContext()
+        {
+        }
+
+        public CompanyDbContext(DbContextOptions<CompanyDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Department> Department { get; set; }
+        public virtual DbSet<Employee> Employee { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Department>(entity =>
+            {
+                entity.Property(e => e.Name).IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Employee>(entity =>
+            {
+                entity.Property(e => e.FirstName).IsUnicode(false);
+
+                entity.Property(e => e.LastName).IsUnicode(false);
+
+                entity.HasOne(d => d.Department)
+                    .WithMany(p => p.Employee)
+                    .HasForeignKey(d => d.DepartmentId)
+                    .HasConstraintName("FK_Employee_Department");
+
+                entity.HasOne(d => d.Manager)
+                    .WithMany(p => p.Subordinate)
+                    .HasForeignKey(d => d.ManagerId)
+                    .HasConstraintName("FK_Employee_Employee");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ CompanyApi/CompanyApi/Models/Employee.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace CompanyApi.Models
+{
+    public partial class Employee
+    {
+        public Employee()
+        {
+            Subordinate = new HashSet<Employee>();
+        }
+
+        [Key]
+        public int EmployeeId { get; set; }
+        [StringLength(15)]
+        public string FirstName { get; set; }
+        [StringLength(15)]
+        public string LastName { get; set; }
+        public int? ManagerId { get; set; }
+        [Column(TypeName = "decimal(6, 2)")]
+        public decimal? Salary { get; set; }
+        [Column(TypeName = "decimal(6, 2)")]
+        public decimal? Bonus { get; set; }
+        public int? DepartmentId { get; set; }
+
+        [ForeignKey(nameof(DepartmentId))]
+        [InverseProperty("Employee")]
+        public virtual Department Department { get; set; }
+        [ForeignKey(nameof(ManagerId))]
+        [InverseProperty(nameof(Employee.Subordinate))]
+        public virtual Employee Manager { get; set; }
+        [InverseProperty(nameof(Employee.Manager))]
+        public virtual ICollection<Employee> Subordinate { get; set; }
+    }
+}
